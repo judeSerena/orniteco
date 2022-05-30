54f9663518b8ab0a2ee0f43736f22f7d3e63cac5
Jude <judeserena@outlook.com>
2022-05-30 19:36:11 +0200

    Afegir mòdul per fetch d'audios

---
+++ PF/modules/fetch.js
@@ -0,0 +1,29 @@
+/*
+XENO-CANTO API DOCS
+https://xeno-canto.org/explore/api
+https://xeno-canto.org/help/search
+*/
+
+export {fetchRecordings};
+
+async function fetchRecordings(bird = 'Inventus forfetchus', minQuality = 'c', minDuration = 5, maxDuration = 10) {
+    //q_gt:c = Quality_GreaterThan:C (recording quality levels go A > E)
+    const endpoint = `https://xeno-canto.org/api/2/recordings?query=${bird.replace(/ /g, '+')}`
+        + `+q_gt:${minQuality}`
+        + `+len:${minDuration}-${maxDuration}`
+        + `+type:song`;
+    
+    let response = await fetch(endpoint);
+
+    if(response.ok) {
+        let data = await response.json();
+        console.log(data.recordings);
+    } else {
+        informFetchError(response, this);
+    }
+}
+
+function informFetchError(response, func) {
+    console.error(`Error: could not fetch at \'${func.name}\'.` +
+      `Reason: ${response.status} (${response.statusText}).`);
+}
