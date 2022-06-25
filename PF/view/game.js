355e68cb1fe97a80f5a2be6bc5a1c38939413b75
Jude <judeserena@outlook.com>
2022-06-25 15:16:16 +0200

    Afegir eventListener per clic a botons de resposta

--- PF/view/game.js
+++ PF/view/game.js
@@ -11,6 +11,7 @@ const language = 'ca';
 
 // Retrieve DOM elements
 const audioElement = document.getElementsByTagName('audio')[0];
+const answerContainer = document.getElementsByClassName('answers')[0];
 const answerButtons = document.querySelectorAll('.answers button');
 
 /**
@@ -30,7 +31,7 @@ function drawQuestion(choices) {
         // access the correct option at choices[0].
         const choiceOrder = [0, 1, 2].sort(() => Math.random() - 0.5);
         answerButtons[0].textContent = choices[choiceOrder[0]].name[language];
-        answerButtons[0].setAttribute('data-correct', 'yes');
+        // answerButtons[0].setAttribute('data-correct', 'yes');////////////////////////////////crec que no caldrà
         answerButtons[1].textContent = choices[choiceOrder[1]].name[language];
         answerButtons[2].textContent = choices[choiceOrder[2]].name[language];
         ///////////////////////////////////////////////////////////////////////////TO-DO: mostrar també imatges.
@@ -44,7 +45,15 @@ function drawQuestion(choices) {
 //a levels.js també hauré de settejar exp a 0 si no es troba exp guardada a localstorage, i segons la exp, mostrar nivells desbloquejats
 genQuestionCandidates(0, drawQuestion);
 
-//eventListeners per al div contenint els botons
-//si el botó recollit en e (event) té l'atribut data-correct = 'yes'
-//llavors suma exp i actualitza-la a localstorage, i mostra que és correcte, i genera nova pregunta si es clica botó de següent
-//else mostra que és incorrecte i genera nova pregunta si es clica botó de següent
+answerContainer.addEventListener('click', e => {
+    if (e.target.classList.contains('btn')){
+        // If the clicked button is the correct choice (index 0)...
+        if (e.target === answerButtons[0]) {
+            alert('correct');
+            //suma exp i actualitza-la a localstorage, i mostra que és correcte, i genera nova pregunta si es clica botó de següent
+        } else {
+            alert('incorrect');
+            //mostra que és incorrecte i genera nova pregunta si es clica botó de següent
+        }
+    }
+});
