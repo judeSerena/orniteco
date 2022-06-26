b2080bb17fa14c3858317a9e34ad089493aa96c6
Jude <judeserena@outlook.com>
2022-06-26 16:25:47 +0200

    Mostrar imatges en les respostes. Millores CSS

--- PF/view/game.js
+++ PF/view/game.js
@@ -16,7 +16,8 @@ const pointBar = document.getElementsByClassName('point-bar')[0];
 const levelTitle = document.querySelector('.game-top-bar h2');
 const audioElement = document.getElementsByTagName('audio')[0];
 const answerContainer = document.getElementsByClassName('answers')[0];
-const answerButtons = document.querySelectorAll('.answers button');
+const answerButtons = document.querySelectorAll('.answers a p');
+const answerButtonsImgs = document.querySelectorAll('.answers a img');
 
 function updatePointBar(){
     pointBar.textContent = getPoints();
@@ -25,29 +26,33 @@ function updatePointBar(){
 /**
  * Draws game interface (with question and 3 possible answers) in view/game.html, from an array of 3 bird names,
  * the first of which is the correct option.
- * @param {String[]} choices Array of 3 objects representing birds.
+ * @param {String[]} birdChoices Array of 3 objects representing birds.
  */
-function drawQuestion(choices) {
+function drawQuestion(birdChoices) {
     // Get recording from first bird (correct choice) in choices.
     // Draw the question via callback when we get the data.
-    fetchRecordings(choices[0].name.sci, minQuality, minDuration, maxDuration, (recordings) => {
+    fetchRecordings(birdChoices[0].name.sci, minQuality, minDuration, maxDuration, (recordings) => {
         // Choose a random recording from the ones available for this species and load it into the <audio> tag
         const randomIndex = Math.floor(Math.random() * recordings.length);
         audioElement.setAttribute('src', recordings[randomIndex].file);
 
-        // Random choice order, stored in a sepparate array so that we can
-        // know that the correct option is always at answerButtons[0] even if
-        // they are displayed in random order on the page.
+        /* Random choice order, stored in a sepparate array so that we know that the correct option
+        is always at answerButtons[0] even if they are displayed in random order on the page.*/
         const choiceOrder = [0, 1, 2].sort(() => Math.random() - 0.5);
-        answerButtons[choiceOrder[0]].textContent = choices[0].name[language];
+        answerButtons[choiceOrder[0]].textContent = birdChoices[0].name[language];
         answerButtons[choiceOrder[0]].setAttribute('data-correct', 'yes');
-        answerButtons[choiceOrder[1]].textContent = choices[1].name[language];
+        // Randomly choose 1 out of 2 possible images for each bird (files named "bird 1.jpg" and "bird 2.jpg")
+        answerButtonsImgs[choiceOrder[0]].src = `./data/imgs/${birdChoices[0].name.sci} ${Math.floor(Math.random() * 2) + 1}.jpg`;
+        console.log(answerButtonsImgs[choiceOrder[0]]);
+        console.log(answerButtonsImgs);
+
+        answerButtons[choiceOrder[1]].textContent = birdChoices[1].name[language];
         answerButtons[choiceOrder[1]].setAttribute('data-correct', 'no');
-        answerButtons[choiceOrder[2]].textContent = choices[2].name[language];
+        answerButtonsImgs[choiceOrder[1]].src = `./data/imgs/${birdChoices[1].name.sci} ${Math.floor(Math.random() * 2) + 1}.jpg`;
+
+        answerButtons[choiceOrder[2]].textContent = birdChoices[2].name[language];
         answerButtons[choiceOrder[2]].setAttribute('data-correct', 'no');
-        ///////////////////////////////////////////////////////////////////////////TO-DO: mostrar també imatges.
-        //els DIVS han de contenir botó + image.
-        //s'haurà d'arreglar els querySelector.
+        answerButtonsImgs[choiceOrder[2]].src = `./data/imgs/${birdChoices[2].name.sci} ${Math.floor(Math.random() * 2) + 1}.jpg`;
     })
 }
 
